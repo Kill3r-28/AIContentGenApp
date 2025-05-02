439caf93ae95434fe0e425eb4f983e8445624e01
Ravikant-0332 <ravikantraipuria1357@gmail.com>
2025-05-02 11:44:23 +0530

    Prototype function to get reponse from gpt is Implemented.

--- content_gen/views.py
+++ content_gen/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from .serializers import PromptSerializer, GptResponseSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from openai import OpenAI
 
 
 class GptRequestView(APIView):
@@ -15,29 +16,48 @@ class GptRequestView(APIView):
         if serializer.is_valid():
             prompt = serializer.validated_data.get("prompt")
             if len(prompt.strip()) > 10:
-                return Response({"message": self.get_response_from_gpt(prompt)}, status=status.HTTP_200_OK)
+                response = self.get_response_from_gpt(prompt)
+
+                if response.get("finish_reason", "") != 'stop':
+                    return Response({"error": "GPT was unable to complete the request due to an internal error or because the output exceeded the maximum token limit."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+                return Response({
+                    "message": response.get("message"),
+                    "input_token": response.get("input_token"),
+                    "output_token": response.get("output_token"),
+                    "total_token": response.get("total_token"),
+                                 }, status=status.HTTP_200_OK)
             else:
                 return Response({"error": "Invalid prompt data"}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({"error": "Invalid request body"}, status=status.HTTP_400_BAD_REQUEST)
 
     def get_response_from_gpt(self, prompt):
-        return "Generated Text"
+
+        client = OpenAI(api_key=settings.OPENAI_API_KEY)
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
+
+        result = {
+            "message": completion.choices[0].message.content,
+            "input_token": completion.usage.prompt_tokens,
+            "output_token": completion.usage.completion_tokens,
+            "total_token": completion.usage.total_tokens,
+            "finish_reason": completion.choices[0].finish_reason
+        }
+
+        return result
 
 
 class PromptView(APIView):
     permission_classes = [IsAuthenticated]
-
-    def get_prompt(self, request_type):
-
-        file_name = {
-            "mcq": "temp.txt",
-            "mcq_cpp": "mcq_cpp.txt",
-            "ca_mcq": "ca_prompt_txt",
-        }
-
-        with open(settings.BASE_DIR / 'prompts' / file_name.get(request_type, 'no_file.txt')) as f:
-            return f.read()
 
     def post(self, request):
         serializer = PromptSerializer(data=request.data)
@@ -50,4 +70,15 @@ class PromptView(APIView):
                 return Response({"error": "Prompt file not found"}, status=status.HTTP_404_NOT_FOUND)
         else:
             return Response({"error": "Invalid Data Passed"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_prompt(self, request_type):
+
+        file_name = {
+            "mcq": "temp.txt",
+            "mcq_cpp": "mcq_cpp.txt",
+            "ca_mcq": "ca_prompt_txt",
+        }
+
+        with open(settings.BASE_DIR / 'prompts' / file_name.get(request_type, 'no_file.txt')) as f:
+            return f.read()
 
