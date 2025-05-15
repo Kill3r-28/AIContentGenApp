4ec63f60c2f221d6127e8ab9134e4fec2a9ec993
Ravikant-0332 <ravikantraipuria1357@gmail.com>
2025-05-15 11:56:06 +0530

    HTTP response code for GPT token limit exceede is fixed. Added Endpoint to verify token

--- api_auth/urls.py
+++ api_auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 app_name = "api_auth"
@@ -7,4 +7,5 @@ app_name = "api_auth"
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
